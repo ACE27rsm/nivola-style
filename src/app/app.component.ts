94204800bdf2d34f226847b40b55a237b6cc4e45
Daniele Salvatori <danielesalvatori@3dprint.sm>
2025-03-10 16:17:57 +0100

    Add Tailwind CSS configuration, implement Locomotive Scroll, and enhance header component with menu functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import LocomotiveScroll from 'locomotive-scroll';
+import { ResizeObserver } from '@juggle/resize-observer';
+
+// * routes
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
@@ -9,6 +13,42 @@ import { HeaderComponent } from './header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nivola-style';
+  scroll: LocomotiveScroll | null = null;
+  @ViewChild('scrollContent') scrollContent: ElementRef | null = null;
+
+  ngOnInit() {
+    this.scroll = new LocomotiveScroll({
+      el: document.querySelector('[data-scroll-container]') as HTMLElement,
+      smooth: true,
+      getDirection: true,
+      lerp: 0.06,
+      scrollFromAnywhere: !1,
+      touchMultiplier: 2.5,
+      class: 'show',
+      smartphone: {
+        smooth: !0,
+      },
+      direction: 'vertical',
+    });
+
+    this.scroll.on('scroll', (event) => {
+      console.log(event);
+    });
+  }
+
+  ngAfterViewInit() {
+    const ro = new ResizeObserver((entries, observer) => {
+      entries.forEach((entry, index) => {
+        const { inlineSize: width, blockSize: height } =
+          entry.contentBoxSize[0];
+        if (this.scroll) {
+          this.scroll.update();
+        }
+      });
+    });
+
+    ro.observe(this.scrollContent?.nativeElement);
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,6 +6,7 @@ import {
   Router,
   RouterModule,
 } from '@angular/router';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-header',
@@ -15,6 +16,7 @@ import {
 })
 export class HeaderComponent {
   currentUrl: string = '';
+  isMenuOpen: boolean = false;
 
   constructor(public location: Location, router: Router) {
     router.events.subscribe((val) => {
@@ -23,5 +25,43 @@ export class HeaderComponent {
         console.log(this.currentUrl);
       }
     });
+  }
+
+  onMenuClick() {
+    this.isMenuOpen = !this.isMenuOpen;
+
+    if (this.isMenuOpen) {
+      gsap.set('#drawer-container', {
+        display: 'block',
+      });
+      gsap.to('#drawer-container .backdrop', {
+        duration: 0.2,
+        opacity: 1,
+        pointerEvents: 'all',
+        ease: 'power2.inOut',
+      });
+      gsap.to('#drawer-container .content', {
+        duration: 0.2,
+        transform: 'translateX(0)',
+        ease: 'power2.inOut',
+      });
+    } else {
+      gsap.to('#drawer-container .backdrop', {
+        duration: 0.2,
+        opacity: 0,
+        pointerEvents: 'none',
+        ease: 'power2.inOut',
+        onComplete: () => {
+          gsap.set('#drawer-container', {
+            display: 'none',
+          });
+        },
+      });
+      gsap.to('#drawer-container .content', {
+        duration: 0.2,
+        transform: 'translateX(100vw)',
+        ease: 'power2.inOut',
+      });
+    }
   }
 }
