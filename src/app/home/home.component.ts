234be01c0e51a3883377daaf8832514e337bdf83
Daniele Salvatori <danielesalvatori@3dprint.sm>
2025-03-11 08:19:46 +0100

    Update project configuration for deployment, add 404 page, and adjust asset paths

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,49 +12,49 @@ import { InfiniteScrollComponent } from '../infinite-scroll/infinite-scroll.comp
 export class HomeComponent {
   brands: IInfiniteScrollItem[] = [
     {
-      src: '/brands/akrapovic.svg',
+      src: 'brands/akrapovic.svg',
       alt: 'Akrapovic',
       className: 'h-full',
       href: 'https://www.akrapovic.com/en',
     },
     {
-      src: '/brands/bardahl.png',
+      src: 'brands/bardahl.png',
       alt: 'Bardahl',
       className: 'h-full',
       href: 'https://www.bardahl.it/',
     },
     {
-      src: '/brands/castrol.png',
+      src: 'brands/castrol.png',
       alt: 'Castrol',
       className: 'h-full',
       href: 'https://www.castrol.com/it_it/italy/home.html',
     },
     {
-      src: '/brands/givi.svg',
+      src: 'brands/givi.svg',
       alt: 'Givi',
       className: 'h-full',
       href: 'https://www.givi.it/',
     },
     {
-      src: '/brands/cellularline.svg',
+      src: 'brands/cellularline.svg',
       alt: 'Cellularline',
       className: 'h-full',
       href: 'https://www.cellularline.com/it-it/',
     },
     {
-      src: '/brands/motul.svg',
+      src: 'brands/motul.svg',
       alt: 'Motul',
       className: 'h-full',
       href: 'https://www.motul.com/it-IT',
     },
     {
-      src: '/brands/rizoma.svg',
+      src: 'brands/rizoma.svg',
       alt: 'Rizoma',
       className: 'h-full',
       href: 'https://www.rizoma.com/',
     },
     {
-      src: '/brands/termignoni.svg',
+      src: 'brands/termignoni.svg',
       alt: 'Termignoni',
       className: 'h-full',
       href: 'https://www.termignoni.it/',
