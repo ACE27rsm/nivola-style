bb66a112fc204aad2ae47456bfcca0e24963ec56
Daniele Salvatori <danielesalvatori@3dprint.sm>
2025-03-11 17:16:17 +0100

    Add new images and implement infinite scroll in About Us component

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,11 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { IInfiniteScrollItem } from '../../types/Types';
+import { InfiniteScrollComponent } from '../infinite-scroll/infinite-scroll.component';
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [CommonModule, InfiniteScrollComponent],
   templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.css'
+  styleUrl: './about-us.component.css',
 })
 export class AboutUsComponent {
-
+  photos: IInfiniteScrollItem[] = [
+    {
+      src: 'photos/A.png',
+      alt: 'A',
+      className: 'h-full rounded-lg',
+      href: '',
+    },
+    {
+      src: 'photos/B.png',
+      alt: 'B',
+      className: 'h-full rounded-lg',
+      href: '',
+    },
+    {
+      src: 'photos/C.png',
+      alt: 'C',
+      className: 'h-full rounded-lg',
+      href: '',
+    },
+    {
+      src: 'photos/D.png',
+      alt: 'D',
+      className: 'h-full rounded-lg',
+      href: '',
+    },
+    {
+      src: 'photos/E.png',
+      alt: 'E',
+      className: 'h-full rounded-lg',
+      href: '',
+    },
+  ];
 }
